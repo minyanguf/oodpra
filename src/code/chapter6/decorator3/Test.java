83c17fa7991451bf20823ce36d4880ad452270a4
ni <ocala352615@gmail.com>
2023-06-18 16:53:01 -0400

    总结decorate 模式 其实 就是通过 abstract component那里 有omponent的field 然后有setcomponent 方法 ， 最后通过 一个object 包着一个object    就是通过filed component 一直包括下去 所以main 运行 最外层的 object。show 时候 会一直通过 super 里的component 一直调用到最里面的object  然后 从最里面开始一直 运行show（）

--- src/code/chapter6/decorator3/Test.java
+++ src/code/chapter6/decorator3/Test.java
@@ -23,37 +23,37 @@ public class Test {
 
         dtx.show();                         //执行形象展示
 
-        System.out.println(" 第二种装扮：");
-
-        LeatherShoes px = new LeatherShoes();//生成皮鞋实例
-        px.decorate(xc);                    //皮鞋装饰小菜
-        
-        Tie ld = new Tie();                 //生成领带实例
-        ld.decorate(px);                    //领带装饰“有皮鞋装饰的小菜”
-        
-        Suit xz = new Suit();               //生成西装实例
-        xz.decorate(ld);                    //西装装饰“有领带皮鞋装饰的小菜”
-
-        xz.show();                          //执行形象展示
-
-        System.out.println(" 第三种装扮：");
-        
-        Sneakers pqx2 = new Sneakers();     //生成球鞋实例
-        pqx2.decorate(xc);                  //球鞋装饰小菜
-        
-        LeatherShoes px2 = new LeatherShoes();//生成皮鞋实例
-        px2.decorate(pqx2);                 //皮鞋装饰“有球鞋装饰的小菜”
-        
-        BigTrouser kk2 = new BigTrouser();  //生成垮裤实例
-        kk2.decorate(px2);                  //垮裤装饰“有皮鞋球鞋装饰的小菜”
-        
-        Tie ld2 = new Tie();                //生成领带实例
-        ld2.decorate(kk2);                  //领带装饰“有垮裤皮鞋球鞋装饰的小菜”
-
-        Strawhat cm2 = new Strawhat();      //生成草帽实例
-        cm2.decorate(ld2);                  //草帽装饰“有领带垮裤皮鞋球鞋装饰的小菜”
-
-        cm2.show();                         //执行形象展示
+//        System.out.println(" 第二种装扮：");
+//
+//        LeatherShoes px = new LeatherShoes();//生成皮鞋实例
+//        px.decorate(xc);                    //皮鞋装饰小菜
+//
+//        Tie ld = new Tie();                 //生成领带实例
+//        ld.decorate(px);                    //领带装饰“有皮鞋装饰的小菜”
+//
+//        Suit xz = new Suit();               //生成西装实例
+//        xz.decorate(ld);                    //西装装饰“有领带皮鞋装饰的小菜”
+//
+//        xz.show();                          //执行形象展示
+//
+//        System.out.println(" 第三种装扮：");
+//
+//        Sneakers pqx2 = new Sneakers();     //生成球鞋实例
+//        pqx2.decorate(xc);                  //球鞋装饰小菜
+//
+//        LeatherShoes px2 = new LeatherShoes();//生成皮鞋实例
+//        px2.decorate(pqx2);                 //皮鞋装饰“有球鞋装饰的小菜”
+//
+//        BigTrouser kk2 = new BigTrouser();  //生成垮裤实例
+//        kk2.decorate(px2);                  //垮裤装饰“有皮鞋球鞋装饰的小菜”
+//
+//        Tie ld2 = new Tie();                //生成领带实例
+//        ld2.decorate(kk2);                  //领带装饰“有垮裤皮鞋球鞋装饰的小菜”
+//
+//        Strawhat cm2 = new Strawhat();      //生成草帽实例
+//        cm2.decorate(ld2);                  //草帽装饰“有领带垮裤皮鞋球鞋装饰的小菜”
+//
+//        cm2.show();                         //执行形象展示
 
 		System.out.println();
 		System.out.println("**********************************************");
