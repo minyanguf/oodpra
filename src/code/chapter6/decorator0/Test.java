abe930156cfd4239b5da4c111bd0973a3f570190
ni <ocala352615@gmail.com>
2023-06-18 16:28:30 -0400

    解释了为什么 d2 operaration 里面的super operation 那边compnent 调用的是d1

--- src/code/chapter6/decorator0/Test.java
+++ src/code/chapter6/decorator0/Test.java
@@ -16,6 +16,7 @@ public class Test {
 
         d1.SetComponent(c);	//首先用d1来包装c
         d2.SetComponent(d1);//再用有来包装d1
+        //这里由于SetComponent   所以d2 里面的compoment 是d1  然后看下面super 时候 就是运行component的operation 所以最后是d1
         d2.Operation();   	//最终执行d2的Operation()
 
 
@@ -53,7 +54,7 @@ abstract class Decorator extends Component {
     //重写Operation()，实际调用component的Operation方法
     public void Operation() {
         if (component != null) {
-            //System.out.println(this.component);
+            //这里d2.operation 调用super。Operation 会来到这里    由于一开始new的时候  Decorator和ConcreteDecoratorB 共享了component fild。 这里d2 的componment 是d1
             component.Operation();
 
         }
