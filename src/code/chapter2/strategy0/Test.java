73acbeac117e3cc4e6772a98b43bd9d4106f14c7
ni <ocala352615@gmail.com>
2023-06-19 15:36:29 -0400

    strategy 和 simple factory 的区别是  大体在父子class 的方法继承是一样的  主要区别是在 context和factoryclass里对 父class的应用   factory 父class作为输出  而 strategy 这里 作为 constroctor paramter的输入

--- src/code/chapter2/strategy0/Test.java
+++ src/code/chapter2/strategy0/Test.java
@@ -15,6 +15,8 @@ public class Test {
 		//由于实例化不同的策略，所以最终在调用
 		//context.contextInterface()时,所
 		//获得的结果就不尽相同
+
+        //strategy 和 simple factory 的区别是  大体在父子class 的方法继承是一样的  主要区别是在 context和factoryclass里对 父class的应用   factory 父class作为输出  而 strategy 这里 作为 constroctor paramter的输入
 	    context = new Context(new ConcreteStrategyA());
 	    context.contextInterface();
 
@@ -65,13 +67,15 @@ class ConcreteStrategyC extends Strategy {
 class Context {
     Strategy strategy;
     //初始化时，传入具体的策略对象
+
+    //strategy 和 simple factory 的区别是  大体在父子class 的方法继承是一样的  主要区别是在 context和factoryclass里对 父class的应用   factory 父class作为输出  而 strategy 这里 作为 constroctor paramter的输入
     public Context(Strategy strategy) {
-        this.strategy = strategy;
+        this.strategy=strategy;
     }
     //上下文接口
     public void contextInterface() {
     	//根据具体的策略对象，调用其算法的方法
-        strategy.algorithmInterface();
+        this.strategy.algorithmInterface();
     }
 }
 
