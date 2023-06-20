f3b5133b70e6fd000e50dea33f2e27b352ce3c1f
ni <ocala352615@gmail.com>
2023-06-19 22:00:54 -0400

    把例子删除了一些 class 更简单的state example

--- src/code/chapter16/state3/Test.java
+++ src/code/chapter16/state3/Test.java
@@ -11,28 +11,33 @@ public class Test {
         
         //紧急项目
         Work emergencyProjects = new Work();
+//        emergencyProjects.setState(new ForenoonState());
         emergencyProjects.setHour(9);
         emergencyProjects.callStateWriteProgram();
-        emergencyProjects.setHour(10);
-        //Work class have state field,  某些条件下callStateWriteProgram 改变了 work里的state的子类具体类型 所以可以发生状态转移
-        //为了改变 work里的state 所以callStateWriteProgram 的method 选哟传入work as parameter
-        emergencyProjects.callStateWriteProgram();
-        emergencyProjects.setHour(12);
-        emergencyProjects.callStateWriteProgram();
-        emergencyProjects.setHour(13);
-        emergencyProjects.callStateWriteProgram();
-        emergencyProjects.setHour(14);
-        emergencyProjects.callStateWriteProgram();
-        emergencyProjects.setHour(17);
-
-        emergencyProjects.setWorkFinished(false);
-        //emergencyProjects.setWorkFinished(true);
-
+        emergencyProjects.setHour(16);
         emergencyProjects.callStateWriteProgram();
         emergencyProjects.setHour(19);
         emergencyProjects.callStateWriteProgram();
-        emergencyProjects.setHour(22);
-        emergencyProjects.callStateWriteProgram();
+
+        //Work class have state field,  某些条件下callStateWriteProgram 改变了 work里的state的子类具体类型 所以可以发生状态转移
+        //为了改变 work里的state 所以callStateWriteProgram 的method 选哟传入work as parameter
+//        emergencyProjects.callStateWriteProgram();
+//        emergencyProjects.setHour(12);
+//        emergencyProjects.callStateWriteProgram();
+//        emergencyProjects.setHour(13);
+//        emergencyProjects.callStateWriteProgram();
+//        emergencyProjects.setHour(14);
+//        emergencyProjects.callStateWriteProgram();
+//        emergencyProjects.setHour(17);
+//
+//        emergencyProjects.setWorkFinished(false);
+//        //emergencyProjects.setWorkFinished(true);
+//
+//        emergencyProjects.callStateWriteProgram();
+//        emergencyProjects.setHour(19);
+//        emergencyProjects.callStateWriteProgram();
+//        emergencyProjects.setHour(22);
+//        emergencyProjects.callStateWriteProgram();
 
 		System.out.println();
 		System.out.println("**********************************************");
@@ -61,68 +66,27 @@ class ForenoonState extends State {
     }
 }
 
-//中午工作状态
 class NoonState extends State {
     public void writeProgram (Work w) {
-        if (w.getHour() < 13)  {
-            System.out.println("当前时间："+ w.getHour() +"点 饿了，午饭；犯困，午休。");
+        if (w.getHour() < 18)  {
+            System.out.println("当前时间："+ w.getHour() +"noon");
         }
         else {
-            w.setState(new AfternoonState());
+            w.setState(new EndState());
+
             w.callStateWriteProgram();
         }
     }
 }
 
-//下午工作状态
-class AfternoonState extends State {
+class EndState extends State {
     public void writeProgram (Work w) {
-        if (w.getHour() < 17) {
-            System.out.println("当前时间："+ w.getHour() +"点 下午状态还不错，继续努力");
-        }
-        else {
-            w.setState(new EveningState());
 
-            w.callStateWriteProgram();
-        }
-   }  
-}
+            System.out.println("end");
 
-//晚间工作状态
-class EveningState extends State {
-    public void writeProgram(Work w)
-    {
-        if (w.getWorkFinished())  
-        {
-            w.setState(new RestState());
-            w.callStateWriteProgram();
-        }
-        else
-        {
-            if (w.getHour() < 21) {
-               System.out.println("当前时间："+ w.getHour() +"点 加班哦，疲累之极");
-            }
-            else {
-              w.setState(new SleepingState());
-              w.callStateWriteProgram();
-            }
-        }
     }
 }
 
-//睡眠状态
-class SleepingState extends State {
-    public void writeProgram(Work w) {
-        System.out.println("当前时间："+ w.getHour() +"点 不行了，睡着了。");
-    }
-}
-
-//下班休息状态
-class RestState extends State {
-    public void writeProgram(Work w) {
-        System.out.println("当前时间："+ w.getHour() +"点 下班回家了");
-    }
-}
 
 //工作类
 class Work {
@@ -130,15 +94,17 @@ class Work {
     private State current;
     
     public Work(){
-        current = new ForenoonState();  
+        current = new ForenoonState();
     }
     //设置状态
     public void setState(State value) {
         this.current = value;
     }
     //写代码的状态
+    //关键是context class 里面有 method call state。handle（this） 然后state class 那边 的method 接收到 context class 的parameter  然后在那边改变 state成另一个子类
     public void callStateWriteProgram() {
         this.current.writeProgram(this);
+//        this.current.writeProgram(this);
     }
 
     //当前的钟点
